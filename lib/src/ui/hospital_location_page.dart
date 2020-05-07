932f0f0b346112215f30f2a6ebf4d370c5156c42
Yukio Arie <yukioarie@gmail.com>
2020-05-07 07:00:19 -0300

    Prepara novas pages para o desenvolvimento

--- lib/src/ui/hospital_location_page.dart
+++ lib/src/ui/hospital_location_page.dart
@@ -4,6 +4,8 @@ class HospitalLocationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
       color: Colors.black,
     );
   }
