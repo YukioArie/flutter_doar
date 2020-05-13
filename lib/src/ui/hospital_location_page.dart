19214dabdeb8b150c1dbc05233f5df11370a35e0
Yukio Arie <yukioarie@gmail.com>
2020-05-13 03:27:05 -0300

    Adiciona Mapa no hospital_location_page

--- lib/src/ui/hospital_location_page.dart
+++ lib/src/ui/hospital_location_page.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HospitalLocationPage extends StatelessWidget {
+class HospitalLocationPage extends StatefulWidget {
+  @override
+  _HospitalLocationPageState createState() => _HospitalLocationPageState();
+}
+
+class _HospitalLocationPageState extends State<HospitalLocationPage> {
+  Set<Marker> markers = new Set<Marker>();
+  double lat = -21.754364;
+  double lng = -41.3322832;
+  GoogleMapController mapController;
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      color: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hemocentros"),
+        centerTitle: true,
+      ),
+      drawer: Drawer(),
+      body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(lat, lng),
+            zoom: 14.0,         
+          ),
+        ),
     );
   }
 }
