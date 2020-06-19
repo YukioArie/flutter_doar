d67d32942a63dab1357e2b4249828e2415542dd5
Yukio Arie <yukioarie@gmail.com>
2020-06-19 03:30:56 -0300

    refaz a tela de login

--- lib/src/ui/login_page.dart
+++ lib/src/ui/login_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_doar/src/ui/register_page.dart';
+import 'package:flutter_doar/src/ui/singup_page.dart';
 
-import 'home_page.dart';
+import 'forgot_password_page.dart';
+import 'main_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -9,226 +10,216 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool box = true;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Color(0xFFcf2d13), Color(0xFFdd6464)])),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 3.0,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(100),
-                    bottomRight: Radius.circular(100),
-                  ),
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Image.asset("assets/DoarLogo.png",
-                          width: 150, height: 150)
-                    ]),
-              ),
-              _TextField(context, "Usuário"),
-              _TextField(context, "Senha"),
-              Row(children: <Widget>[
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 16, left: 25),
-                    child: Checkbox(
-                        activeColor: Colors.white,
-                        checkColor: Colors.grey,
-                        focusColor: Colors.black,
-                        value: box,
-                        onChanged: (value) {
-                          setState(() {
-                            box = value;
-                          });
-                        }),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 13,
-                  ),
-                  child: Text(
-                    "Lembrar?",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 13, left: 60),
-                  child: GestureDetector(
-                    onTap: () {
-                      print("aqqqqqqqqq");
-                    },
-                    child: Text(
-                      "Esqueceu a senha?",
-                      style: TextStyle(
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                ),
-              ]),
-              Container(
-                padding: EdgeInsets.only(top: 5),
-                width: MediaQuery.of(context).size.width / 1.2,
-                height: 50,
-                child: RaisedButton(
-                  color: Colors.white,
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => HomePage()));
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Entre com",
-                      style: TextStyle(fontSize: 20),
-                    )
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(right: 30, top: 15),
-                    child: Container(
-                      height: 50,
-                      width: 70,
-                      child: FlatButton(
-                        color: Color(0xFF3a559a),
-                        onPressed: () {},
-                        child: Text(
-                          "f",
-                          style: TextStyle(fontSize: 40, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 30, top: 15),
-                    child: Container(
-                      height: 50,
-                      width: 70,
-                      child: FlatButton(
-                        color: Color(0xFFd94738),
-                        onPressed: () {},
-                        child: Text(
-                          "G",
-                          style: TextStyle(fontSize: 40, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 5, top: 15),
-                    child: Container(
-                      height: 50,
-                      width: 70,
-                      child: FlatButton(
-                        color: Color(0xFF4ea6ea),
-                        onPressed: () {},
-                        child: Text(
-                          "t",
-                          style: TextStyle(fontSize: 40, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 15, left: 65),
-                    child: Text(
-                      "Não tem conta?",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 15, left: 8),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => RegisterPage()));
+  bool lembrar = false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController senhaController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-                        print("aqaaa");
-                      },
-                      child: Text(
-                        "Cadastre-se",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Container(height: 50),
-            ],
+  Widget _lblPassword() {
+    return Theme(
+      data: ThemeData(primaryColor: Colors.black45),
+      child: TextFormField(
+        validator: (value) {
+          if (value.isEmpty) {
+            return "Insira senha!";
+          }
+        },
+        controller: senhaController,
+        obscureText: true,
+        keyboardType: TextInputType.emailAddress,
+        style: kHintTextStyle,
+        decoration: InputDecoration(
+          errorStyle: kHintTextStyle,
+          border: OutlineInputBorder(),
+          contentPadding: EdgeInsets.only(top: 14.0),
+          prefixIcon: Icon(
+            Icons.lock,
+            color: Colors.black45,
+            size: 22.0,
+          ),
+          labelText: "Senha",
+          labelStyle: kHintTextStyle,
+        ),
+      ),
+    );
+  }
+
+  Widget _lblEmail() {
+    return Theme(
+      data: ThemeData(primaryColor: Colors.black45),
+      child: TextFormField(
+        validator: (value) {
+          if (value.isEmpty) {
+            return "Insira email!";
+          }
+        },
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        style: TextStyle(color: Colors.black45, fontFamily: 'OpenSans'),
+        decoration: InputDecoration(
+          errorStyle: kHintTextStyle,
+          border: OutlineInputBorder(),
+          contentPadding: EdgeInsets.only(top: 14.0),
+          prefixIcon: Icon(
+            Icons.email,
+            color: Colors.black45,
+            size: 22.0,
+          ),
+          labelText: "Email",
+          labelStyle: TextStyle(color: Colors.black45, fontFamily: 'OpenSans'),
+        ),
+      ),
+    );
+  }
+
+  Widget _lblForgotPassword() {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ForgotPasswordPage()),
+        ),
+        child: Text(
+          "Esqueceu a senha?",
+          style: kLabelStyle,
+        ),
+      ),
+    );
+  }
+
+  Widget _rememberMeCheckBox() {
+    return Container(
+      height: 20.0,
+      child: Row(
+        children: <Widget>[
+          Theme(
+              data: ThemeData(unselectedWidgetColor: Colors.black45),
+              child: Checkbox(
+                value: lembrar,
+                onChanged: (value) {
+                  setState(() {
+                    lembrar = value;
+                  });
+                },
+                checkColor: Colors.white,
+                activeColor: Colors.black45,
+              )),
+          Text(
+            "Lembrar?",
+            style: kLabelStyle,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _lblLogin() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            return Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => MainPage()));
+          }
+        },
+        color: Colors.white,
+        elevation: 5.0,
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Text(
+          "LOGIN",
+          style: TextStyle(
+            color: Colors.black45,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
           ),
         ),
       ),
     );
   }
-}
 
-Widget _TextField(BuildContext context, String texto) {
-  return Container(
-    padding: EdgeInsets.only(top: 32),
-    child: Column(
-      children: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width / 1.2,
-          height: 50,
-          padding: EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 5,
-                )
-              ]),
-          child: TextField(
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              icon: Icon(
-                Icons.verified_user,
-                color: Colors.grey,
-              ),
-              hintText: texto,
+  Widget _lblSingup() {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(builder: (context) => SingupPage())),
+      child: RichText(
+        text: TextSpan(children: [
+          TextSpan(
+            text: "Não tem Conta? ",
+            style: TextStyle(
+              color: Colors.black45,
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
             ),
           ),
+          TextSpan(
+              text: "Cadastre",
+              style: TextStyle(
+                color: Colors.black45,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ))
+        ]),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Form(
+      key: _formKey,
+      child: Container(
+        height: double.infinity,
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsets.symmetric(
+            horizontal: 40.0,
+            vertical: 80.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: 150,
+                width: 150,
+                child: Image.asset("assets/DoarLogo.png"),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              _lblEmail(),
+              SizedBox(height: 30.0),
+              _lblPassword(),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+              ),
+              _lblForgotPassword(),
+              _rememberMeCheckBox(),
+              _lblLogin(),
+              _lblSingup(),
+            ],
+          ),
         ),
-      ],
-    ),
-  );
+      ),
+    ));
+  }
 }
+
+final kHintTextStyle = TextStyle(
+   color: Colors.black45,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.black45,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
