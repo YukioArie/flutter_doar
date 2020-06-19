6c0fba5d6a7bab68a299efa723249c5cd7f9c803
Yukio Arie <yukioarie@gmail.com>
2020-06-19 04:17:23 -0300

    Adiciona navegação na tela de loading

--- lib/src/ui/loading_page.dart
+++ lib/src/ui/loading_page.dart
@@ -1,8 +1,24 @@
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_doar/src/ui/login_page.dart';
 
+class LoadingPage extends StatefulWidget {
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
 
-class LoadingPage extends StatelessWidget {
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  void initState() {
+    super.initState();
+    _navigate();
+  }
+
+  _navigate() async {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,10 +26,12 @@ class LoadingPage extends StatelessWidget {
         child: Container(
           width: double.maxFinite,
           height: double.maxFinite,
-          child: FlareActor("assets/LogoLoading.flr", animation: "animate",),
+          child: FlareActor(
+            "assets/LogoLoading.flr",
+            animation: "animate",
+          ),
         ),
       ),
-      
     );
   }
 }
