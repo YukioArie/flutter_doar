5ea65091cee4b857df69e87031677172451432c5
Yukio Arie <yukioarie@gmail.com>
2020-07-06 13:10:27 -0300

    refaz a page de hospital e deleta pages n utilizados

--- lib/src/ui/hospital_page.dart
+++ lib/src/ui/hospital_page.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_doar/src/components/hospital_cards.dart';
 
 class HospitalPage extends StatefulWidget {
   @override
@@ -7,77 +8,28 @@ class HospitalPage extends StatefulWidget {
 }
 
 class _HospitalPageState extends State<HospitalPage> {
-  List<Widget> _pages = [HospitalListPage(), HospitalLocationPage()];
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: _pages.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              " Doar",
-              style: TextStyle(
-                fontFamily: "BDP Birgula",
-                fontSize: 40,
-              ),
-            ),
-            backgroundColor: Color(0xFFcf2d13),
-            bottom: TabBar(tabs: [Tab(text: "Hemocentros"), Tab(text: "Localizar Hemocentros")]),
-          ),
-          body: TabBarView(children: _pages),
-        ),
-    );
-  }
-}
-
-class HospitalListPage extends StatefulWidget {
-  @override
-  _HospitalListPageState createState() => _HospitalListPageState();
-}
-
-class _HospitalListPageState extends State<HospitalListPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueAccent,
-    );
-  }
-}
-
-class HospitalLocationPage extends StatefulWidget {
-  @override
-  _HospitalLocationPageState createState() => _HospitalLocationPageState();
-}
-
-class _HospitalLocationPageState extends State<HospitalLocationPage> {
-  Set<Marker> markers = new Set<Marker>();
-  double lat = -21.754364;
-  double lng = -41.3322832;
-  GoogleMapController mapController;
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final Marker marker = Marker(
-      markerId: MarkerId("123456"),
-      position: LatLng(-21.7438157, -41.3336036),
-      infoWindow: InfoWindow(title: "Hemocentro"),
-    );
-    setState(() {
-      markers.add(marker);
-    });
-
     return Scaffold(
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(lat, lng),
-          zoom: 14.0,
-        ),
-        markers: markers,
+    appBar: AppBar(
+        title: Text(" Doar", style: TextStyle(fontFamily: "BDP Birgula", fontSize: 40,),),
+        backgroundColor: Color(0xFFcf2d13),
       ),
+    body: FutureBuilder<QuerySnapshot>(future: Firestore.instance.collection("hemocentros").getDocuments(),
+    builder: (context, snapshot){
+      if(!snapshot.hasData){
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }else{
+        return ListView(
+          children: snapshot.data.documents.map((doc){
+            return HospitalCards(doc);
+          }
+          ).toList(),
+        );
+      }
+    }),
     );
   }
 }
