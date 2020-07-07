dcf9730b67f1fd4bf6efd44f7002c53b0f474aef
Yukio Arie <yukioarie@gmail.com>
2020-07-07 01:25:17 -0300

    faz a a page de profile

--- lib/src/ui/profile_page.dart
+++ lib/src/ui/profile_page.dart
@@ -1,12 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_doar/src/ui/apoiadores_page.dart';
+import 'package:flutter_doar/src/ui/historic_donations_page.dart';
+import 'package:flutter_doar/src/ui/rank_body_page.dart';
+import 'package:flutter_doar/src/ui/register_donation_page.dart';
+import 'package:flutter_doar/src/ui/reward_page.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class ProfilePage extends StatelessWidget {
+  Widget _optionsBtn(
+      BuildContext context, String title, Icon iconData, Widget goToPage) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => goToPage));
+      },
+      child: Container(
+        color: Colors.transparent,
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+            iconData,
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container( 
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      color: Colors.blue
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 7,
+              color: Colors.grey[300],
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 48, 10, 15),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        color: Colors.blue, shape: BoxShape.circle),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5, top: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Bem Vindo, Maria",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Pontos: 5000",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  _optionsBtn(context, "Histórico de doação",
+                      Icon(FontAwesome.history), HistoricDonationsPage()),
+                  _optionsBtn(context, "Ranking doador",
+                      Icon(FontAwesome.trophy), RankBodyPage()),
+                  _optionsBtn(context, "Registrar doação",
+                      Icon(FontAwesome.qrcode), RegisterDonationPage()),
+                  _optionsBtn(context, "Recompensas", Icon(FontAwesome.gift),
+                      RewardPage()),
+                  _optionsBtn(context, "Apoiadores", Icon(FontAwesome.building),
+                      ApoiadoresPage()),
+                  Divider(
+                    color: Colors.grey,
+                  ),
+                  _optionsBtn(context, "Sair", Icon(FontAwesome.sign_out),
+                      ApoiadoresPage()),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
