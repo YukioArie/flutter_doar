1d400f8ee915a076cba968d60fd59592f9764752
Yukio Arie <yukioarie@gmail.com>
2020-07-07 04:31:41 -0300

    altera estilização da rank page

--- lib/src/ui/rank_body_page.dart
+++ lib/src/ui/rank_body_page.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_doar/src/ui/rank_card.dart';
+import 'package:flutter_doar/src/components/rank_card.dart';
 
 class RankBodyPage extends StatefulWidget {
   @override
@@ -8,41 +8,6 @@ class RankBodyPage extends StatefulWidget {
 }
 
 class _RankBodyPageState extends State<RankBodyPage> {
-  final List<Widget> _pages = [
-      RankUsersPage(),
-      RankCompaniesPage(),
-  ];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: _pages.length, 
-      child: Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Rank"),
-        backgroundColor: Color(0xFFcf2d13),
-        bottom: TabBar(tabs: [
-          Tab(text: "Doadores"),
-          Tab(text: "Empresas")
-        ]),
-      ),
-    
-      body:TabBarView(
-          children: _pages, 
-          ),
-    ),);
-  }
-}
-
-
-class RankUsersPage extends StatefulWidget {
-  @override
-  _RankUsersPageState createState() => _RankUsersPageState();
-}
-
-class _RankUsersPageState extends State<RankUsersPage> {
   var users = [];
   @override
   void initState() {
@@ -56,228 +21,109 @@ class _RankUsersPageState extends State<RankUsersPage> {
     });
   }
 
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 3.0,
-          color: Colors.grey[200],
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                  
-                  top: 60,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "2 Lugar",
-                      style: TextStyle(fontSize: 25.0),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5),
-                      width: 100,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset("assets/DoarLogo.png"),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 10),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "1 Lugar",
-                      style: TextStyle(fontSize: 25.0),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5),
-                      width: 100,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset("assets/DoarLogo.png"),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(
-                
-                  top: 60,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "2 Lugar",
-                      style: TextStyle(fontSize: 25.0),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5),
-                      width: 100,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset("assets/DoarLogo.png"),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        
-        Expanded(
-            child: Container(
-          decoration: BoxDecoration(color: Colors.grey[200]),
-          padding: EdgeInsets.only(bottom: 2),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width,
-          child: ListView(
-            children: users.map((ranking) {
-              return Padding(
-                  padding: EdgeInsets.all(3),
-                  child: RankCard(
-                      profilePicture: ranking['profilePicture'],
-                      userName: ranking['userName'],
-                      pointsRank: ranking['pointsRank'],
-                      positionRank: ranking['positionRank']));
-            }).toList(),
-          ),
-        ))
-      ],
-    );
-  }
-}
-
-class RankCompaniesPage extends StatefulWidget {
   @override
-  _RankCompaniesPageState createState() => _RankCompaniesPageState();
-}
-
-class _RankCompaniesPageState extends State<RankCompaniesPage> {
-  var users = [];
-  @override
-  void initState() {
-    super.initState();
-    Dio()
-        .get('http://www.mocky.io/v2/5eb51d5e0e0000330b081f94')
-        .then((resposta) {
-      setState(() {
-        users = resposta.data;
-      });
-    });
-  }
-
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height / 3.0,
-          color: Colors.grey[200],
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(
-                  
-                  top: 60,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "2 Lugar",
-                      style: TextStyle(fontSize: 25.0),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5),
-                      width: 100,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset("assets/DoarLogo.png"),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 10),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "1 Lugar",
-                      style: TextStyle(fontSize: 25.0),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5),
-                      width: 100,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset("assets/DoarLogo.png"),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(
-                
-                  top: 60,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "2 Lugar",
-                      style: TextStyle(fontSize: 25.0),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 5),
-                      width: 100,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.asset("assets/DoarLogo.png"),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Rank"),
+          backgroundColor: Color(0xFFcf2d13),
         ),
-        
-        Expanded(
-            child: Container(
-          decoration: BoxDecoration(color: Colors.grey[200]),
-          padding: EdgeInsets.only(bottom: 2),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width,
-          child: ListView(
-            children: users.map((ranking) {
-              return Padding(
-                  padding: EdgeInsets.all(3),
-                  child: RankCard(
-                      profilePicture: ranking['profilePicture'],
-                      userName: ranking['userName'],
-                      pointsRank: ranking['pointsRank'],
-                      positionRank: ranking['positionRank']));
-            }).toList(),
-          ),
-        ))
-      ],
-    );
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 3.0,
+              color: Colors.grey[200],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 60,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "2 Lugar",
+                          style: TextStyle(fontSize: 25.0),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          width: 100,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: Image.asset("assets/DoarLogo.png"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "1 Lugar",
+                          style: TextStyle(fontSize: 25.0),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          width: 100,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: Image.asset("assets/DoarLogo.png"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 60,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "2 Lugar",
+                          style: TextStyle(fontSize: 25.0),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          width: 100,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: Image.asset("assets/DoarLogo.png"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(color: Colors.grey[200]),
+              padding: EdgeInsets.only(bottom: 2),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              child: ListView(
+                children: users.map((ranking) {
+                  return Padding(
+                      padding: EdgeInsets.all(3),
+                      child: RankCard(
+                          profilePicture: ranking['profilePicture'],
+                          userName: ranking['userName'],
+                          pointsRank: ranking['pointsRank'],
+                          positionRank: ranking['positionRank']));
+                }).toList(),
+              ),
+            ))
+          ],
+        ));
   }
 }
-
